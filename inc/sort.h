ea0e63d31c8c2d0f4771cb2eadc340cac1ec1cf8
Albert Shefner <solaris945@gmail.com>
2024-01-04 15:02:40 +0300

    Implemented selection sort

--- inc/sort.h
+++ inc/sort.h
@@ -59,5 +59,63 @@ void TEMPLATE(sort_compare_bubble, T)(T *array, int count, int (*compare)(T, T))
     }
 }
 
+// =========================================
+//              Selection sort
+// =========================================
+
+void TEMPLATE(sort_selection, T)(T *array, int count)
+{
+    for (int current_index = 0; current_index < count; current_index++)
+    {
+        T min = array[current_index];
+        T min_index = current_index;
+        for (int min_search_index = current_index + 1; min_search_index < count; min_search_index++)
+        {
+            if (array[min_search_index] < min)
+            {
+                min = array[min_search_index];
+                min_index = min_search_index;
+            }
+        }
+        SWAP(array[current_index], array[min_index], T);
+    }
+}
+
+void TEMPLATE(sort_reverse_selection, T)(T *array, int count)
+{
+    for (int current_index = 0; current_index < count; current_index++)
+    {
+        T max = array[current_index];
+        T max_index = current_index;
+        for (int max_search_index = current_index + 1; max_search_index < count; max_search_index++)
+        {
+            if (array[max_search_index] > max)
+            {
+                max = array[max_search_index];
+                max_index = max_search_index;
+            }
+        }
+        SWAP(array[current_index], array[max_index], T);
+    }
+}
+
+void TEMPLATE(sort_compare_selection, T)(T *array, int count, int (*compare)(T, T))
+{
+    for (int current_index = 0; current_index < count; current_index++)
+    {
+        T min = array[current_index];
+        T min_index = current_index;
+        for (int min_search_index = current_index + 1; min_search_index < count; min_search_index++)
+        {
+            if (compare(array[min_search_index], min) < 0)
+            {
+                min = array[min_search_index];
+                min_index = min_search_index;
+            }
+        }
+        SWAP(array[current_index], array[min_index], T);
+    }
+}
+
 #undef SWAP
 #undef T
--- test.c
+++ test.c
@@ -4,17 +4,19 @@
 #define T int
 #include "inc/sort.h"
 
+#define len(x) sizeof(x) / sizeof(*x)
+
 int comp(int a, int b)
 {
-    return a - b;
+    return b - a;
 }
 
 int main()
 {
-    int arr[] = {4, 5, 2, 3, 9, 8, 4, 5, 10, 11};
-    sort_bubble_int(arr, 10);
+    int arr[] = {4, 5, 2, 3, 9, 8, 4, 5, 10, 11, 99, 2, 7, 54, 2, -3, -45, -223, 4, -8, 44, 0};
+    sort_compare_selection_int(arr, len(arr), comp);
 
-    for(int i = 0; i < 10; i++)
+    for(size_t i = 0; i < len(arr); i++)
     {
         printf("%d ", arr[i]);
     }
